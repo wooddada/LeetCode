0119b143541fc02464befcdb9a44621f65f740f6
wooddada <125844567+wooddada@users.noreply.github.com>
2023-07-19 11:51:50 +0900

    Time: 27 ms (99.90%), Space: 13.3 MB (66.60%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 :
-            return False
-        if x ==0 or len(str(x))==1:
-            return True
+
         a = str(x)
         for i in range(len(a)//2):
             if a[i] !=a[len(a)-1-i]:
